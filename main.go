ab7a9e03d0ccf6389587721a41f7d150ac174fbb
Roman <roman@tronenko.net>
2017-03-02 12:30:07 +0800

    Added some colors; json/toml outputs

--- main.go
+++ main.go
@@ -11,7 +11,10 @@ import (
 	"strings"
 	"time"
 
+	json "github.com/nwidger/jsoncolor"
+
 	"github.com/BurntSushi/toml"
+	"github.com/fatih/color"
 )
 
 const (
@@ -44,7 +47,7 @@ func check(e error) {
 	}
 }
 
-func findTattosFromFrontmatter(r io.Reader) (tattoo, error) {
+func extractTomlStr(r io.Reader) (string, error) {
 	scanner := bufio.NewScanner(r)
 	scanner.Scan()
 	delim := scanner.Text()
@@ -57,22 +60,12 @@ func findTattosFromFrontmatter(r io.Reader) (tattoo, error) {
 			tomlStr += text + "\n"
 		}
 	}
-	//fmt.Print(tomlStr)
-
-	var tat tattoo
-	_, er := toml.Decode(tomlStr, &tat)
-	if er != nil {
-		return tat, er
-	}
-	//fmt.Printf("%s (%s)\n", tomlObj.Title, tomlObj.Image)
-	fmt.Println()
-	fmt.Println(tat.Title)
-	return tat, nil
+	return tomlStr, nil
 }
 
 func timeTrack(start time.Time, name string) {
 	elapsed := time.Since(start)
-	log.Printf("%s took %s", name, elapsed)
+	color.Green("\n%s took %s\n\n", name, elapsed)
 }
 
 // WriteCounter counts the number of bytes written to it.
@@ -90,9 +83,15 @@ func (wc *WriteCounter) Write(p []byte) (int, error) {
 	return n, nil
 }
 
+var existingFiles = 0
+var downloadedFiles = 0
+var proceedFiles = 0
+var brokenFiles []string
+
 func downloadFileFromIPFS(filepath, ipfsHash string) (err error) {
 	if _, er := os.Stat(filepath); !os.IsNotExist(er) {
-		fmt.Println("File already exists. Continue...")
+		existingFiles++
+		color.Blue("File already exists. Continue...")
 		return er
 	}
 	defer timeTrack(time.Now(), "downloading "+ipfsHash)
@@ -104,7 +103,8 @@ func downloadFileFromIPFS(filepath, ipfsHash string) (err error) {
 	}
 	resp, err := client.Get(ipfsHost + ipfsHash)
 	if err != nil {
-		fmt.Print(err)
+		color.New(color.FgRed).Add(color.Underline).Println(err)
+		brokenFiles = append(brokenFiles, ipfsHash)
 		return err
 	}
 	defer resp.Body.Close()
@@ -112,7 +112,8 @@ func downloadFileFromIPFS(filepath, ipfsHash string) (err error) {
 	// Create the file
 	out, err := os.Create(filepath)
 	if err != nil {
-		fmt.Print(err)
+		color.New(color.FgRed).Add(color.Underline).Println(err)
+		brokenFiles = append(brokenFiles, ipfsHash)
 		return err
 	}
 	defer out.Close()
@@ -120,7 +121,8 @@ func downloadFileFromIPFS(filepath, ipfsHash string) (err error) {
 	src := io.TeeReader(resp.Body, &WriteCounter{})
 	_, err = io.Copy(out, src)
 	if err != nil {
-		fmt.Print("Copy error:", err)
+		color.New(color.FgRed).Add(color.Underline).Println(err)
+		brokenFiles = append(brokenFiles, ipfsHash)
 		return err
 	}
 
@@ -143,23 +145,51 @@ func main() {
 				check(err)
 				defer file.Close()
 
-				tat, _ := findTattosFromFrontmatter(file)
+				tatTomlStr, fmEr := extractTomlStr(file)
 
+				//fmt.Print(tomlStr)
+				var tat tattoo
+				_, er := toml.Decode(tatTomlStr, &tat)
+				if er != nil {
+					log.Println()
+					color.New(color.FgRed).Add(color.Underline).Println(er.Error())
+					return er
+				}
+				if fmEr != nil {
+					log.Println()
+					color.New(color.FgRed).Add(color.Underline).Println(fmEr.Error())
+					return fmEr
+				}
+				//fmt.Printf("%s (%s)\n", tomlObj.Title, tomlObj.Image)
+				fmt.Println() // Mix up foreground and background colors, create new mixes!
+				color.New(color.FgRed).Add(color.BgWhite).Println(tat.Title)
+				tatJSON, _ := json.MarshalIndent(tat, "", "")
+				log.Println(string(tatJSON))
+				color.New(color.Bold).Add(color.Italic).Println(tatTomlStr)
 				filePrefix := strings.Replace(tat.MadeDate[:10], "-", ".", -1)
 				filePrefix += " - " + tat.Title + " @" + tat.MadeAtShop
 				os.Mkdir(outputDefault+filePrefix, os.ModePerm)
 				filePrefix += "/" + filePrefix
 				if len(tat.ImageIpfs) > 0 {
 					fmt.Println(tat.ImageIpfs)
-					downloadFileFromIPFS(outputDefault+filePrefix+tat.ImageIpfs+".jpg", tat.ImageIpfs)
+					if downloadFileFromIPFS(outputDefault+filePrefix+tat.ImageIpfs+".jpg", tat.ImageIpfs) == nil {
+						downloadedFiles++
+					}
 				}
 				for _, ipfsHash := range tat.ImagesIpfs {
 					fmt.Println(ipfsHash)
-					downloadFileFromIPFS(outputDefault+filePrefix+ipfsHash+".jpg", ipfsHash)
+
+					if nil == downloadFileFromIPFS(outputDefault+filePrefix+ipfsHash+".jpg", ipfsHash) {
+						downloadedFiles++
+					}
 				}
 
 				return nil
 			})
 	}()
 	<-c
+
+	color.Green("Downloaded files: ", downloadedFiles)
+	color.Green("Existing files: ", existingFiles)
+	color.Red("Failed files: ", len(brokenFiles))
 }
